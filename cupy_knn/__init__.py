1f683da9affdb4d09c58791424ff7b907b462b68
Felix Igelbrink <felix.igelbrink@uni-osnabrueck.de>
2024-07-31 21:21:37 +0200

    replaced setup.py with pyproject-based setup. Added mandatory extra for default cupy and with pre-compiled wheels as they have different names in pip

--- cupy_knn/__init__.py
+++ cupy_knn/__init__.py
@@ -1,2 +1,12 @@
-from ._version import __version__
+try:
+    from importlib.metadata import version
+
+    __version__ = version("cupy_knn")
+except Exception:  # pragma: no cover # pylint: disable=broad-exception-caught
+    try:
+        from ._version import __version__, __version_tuple__
+    except ImportError:
+        __version__ = '0.0.0'
+        __version_tuple__ = (0, 0, 0)
+        
 from .lbvh_index import LBVHIndex
