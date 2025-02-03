e39d7aca71ff1a652e651e459c7d4ef6d880b65e
Felix Igelbrink <felix.igelbrink@uni-osnabrueck.de>
2025-02-03 17:19:25 +0100

    improved setup and updated readme

--- examples/benchmark_knn_query.py
+++ examples/benchmark_knn_query.py
@@ -6,7 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Benchmark knn queries on a dataset')
 parser.add_argument("input_file", type=str, help='An input ply-file used for the benchmark')
-parser.add_argument("-k", "--knn", type=int, nargs='+', default=16,
+parser.add_argument("-k", "--knn", type=int, nargs='+', default=[16],
                     help='The number of nearest neighbors to find for each point in the input file')
 parser.add_argument("-r", "--radius", type=float, default=None,
                     help='The maximum radius the search the nearest neighbors in')
@@ -31,7 +31,6 @@ print(f"Run times for {points.shape[0]} queries with k={args.knn} and r={args.ra
 
 # build the index
 print(benchmark(lbvh.build, (points,), n_repeat=10, n_warmup=1))
-
 
 for ki in args.knn:
     # prepare the index for knn search with K=16
